6821565547356e9b6b9cded8e3f7c9f2deecce4a
Praveen Sinnur <pbsinnur@gmail.com>
2019-10-17 08:28:33 +0530

    first commit

---
+++ app/src/main/java/com/example/lsd_trippers/MainPageActivity.java
@@ -0,0 +1,52 @@
+package com.example.lsd_trippers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+
+        Button mLogout = findViewById(R.id.logout);
+        Button mfindUser = findViewById(R.id.findUser);
+
+        mfindUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), FindUserActivity.class));
+            }
+        });
+
+
+        mLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+
+        getPermissions();
+    }
+
+    private void getPermissions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[]{Manifest.permission.WRITE_CONTACTS, Manifest.permission.READ_CONTACTS}, 1);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/lsd_trippers/UserObject.java
@@ -0,0 +1,18 @@
+package com.example.lsd_trippers;
+
+public class UserObject {
+    private String name,
+                   phone;
+    public UserObject(String name,String phone){
+        this.name=name;
+        this.phone=phone;
+    }
+
+
+
+    public String getPhone() { return phone; }
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
+
+}
